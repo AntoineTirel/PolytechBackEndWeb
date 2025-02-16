38ad65014f66750388cca4fe7141772c43e754c4
Antoine Tirel <antoine.tirel@lilo.org>
2025-02-16 16:36:29 +0100

    feature(cours): adding TP2 correction

--- TP/corriges/pokemon/src/main/kotlin/com/example/pokemon/controller/PokedexController.kt
+++ TP/corriges/pokemon/src/main/kotlin/com/example/pokemon/controller/PokedexController.kt
@@ -5,8 +5,10 @@ import com.example.pokemon.model.Type
 import com.example.pokemon.model.TypeName
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,7 +21,7 @@ import java.net.http.HttpResponse
 @RestController
 @RequestMapping("/pokemon")
 class PokedexController {
-    val pokemonList = listOf(
+    val pokemonList = mutableListOf(
         Pokemon(1, "Bulbizarre", "Un Pokemon bulbe vivant dans les champs", 1, Type(TypeName.Grass, TypeName.Poison)),
         Pokemon(152, "Germignon", "Un petit germe trop mignon", 2, Type(TypeName.Grass)),
         Pokemon(494, "Victini", "Une victoire a portee de main", 5, Type(TypeName.Fire, TypeName.Psychic)),
@@ -30,23 +32,56 @@ class PokedexController {
         return ResponseEntity.ok().body(pokemonList)
     }
 
-    /**
-     * Uncomment the following as you progress through the TD
-     *
-     *
-    @GetMapping
+
+    @GetMapping("/id/{id}")
     fun getPokemonById(
-        @RequestParam("id") id: Int
-    ): Pokemon {
-//TODO
+        @PathVariable id: Int
+    ): ResponseEntity<Pokemon> {
+        val pokemon = pokemonList.find { it.id == id }
+        if (pokemon != null) {
+            return ResponseEntity.ok().body(pokemon)
+        }
+        throw ResponseStatusException(HttpStatus.NOT_FOUND)
     }
 
+    @GetMapping("/generation/{generation}")
+    fun getPokemonByGeneration(
+        @PathVariable generation: Int
+    ): List<Pokemon> {
+        return pokemonList.filter { it.generation == generation }
+    }
 
     @PostMapping
     fun createPokemon(
         @RequestBody pokemon: Pokemon
-    ): Unit {
-        //TODO
+    ): ResponseEntity<Pokemon> {
+        if (pokemonList.find { it.id == pokemon.id } != null) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "the Pokemon already exists")
+        }
+        pokemonList.add(pokemon)
+        return ResponseEntity(pokemon, HttpStatus.CREATED)
     }
-    */
+
+    @PatchMapping("/description/{id}")
+    fun editDescription(
+        @PathVariable id: Int,
+        @RequestBody description: String
+    ): ResponseEntity<Pokemon> {
+        val pokemonToUpdate = pokemonList.find { it.id == id }
+        if (pokemonToUpdate == null) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Pokemon with id: $id not found, update impossible")
+        }
+        val updatedPokemon = pokemonToUpdate.copy(description = description)
+        pokemonList.removeIf { it.id == id }
+        pokemonList.add(updatedPokemon)
+        return ResponseEntity.ok().body(updatedPokemon)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePokemon(
+        @PathVariable id: Int
+    ): ResponseEntity.HeadersBuilder<*> {
+        pokemonList.removeIf { it.id == id }
+        return ResponseEntity.noContent()
+    }
 }
