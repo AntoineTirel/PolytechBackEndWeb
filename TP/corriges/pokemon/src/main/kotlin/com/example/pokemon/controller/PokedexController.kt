0726c15b74966f5432f97e892d5b57516d677ab4
Antoine Tirel <antoine.tirel@lilo.org>
2025-03-11 23:02:49 +0100

    feature(cours): adding subject for the last TP

--- TP/corriges/pokemon/src/main/kotlin/com/example/pokemon/controller/PokedexController.kt
+++ TP/corriges/pokemon/src/main/kotlin/com/example/pokemon/controller/PokedexController.kt
@@ -1,52 +1,99 @@
 package com.example.pokemon.controller
 
-import com.example.pokemon.model.Pokemon
+import com.example.pokemon.model.PokemonDTO
 import com.example.pokemon.model.Type
 import com.example.pokemon.model.TypeName
+import com.example.pokemon.service.PokemonService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.server.ResponseStatusException
-import java.net.http.HttpResponse
 
 @RestController
 @RequestMapping("/pokemon")
-class PokedexController {
-    val pokemonList = listOf(
-        Pokemon(1, "Bulbizarre", "Un Pokemon bulbe vivant dans les champs", 1, Type(TypeName.Grass, TypeName.Poison)),
-        Pokemon(152, "Germignon", "Un petit germe trop mignon", 2, Type(TypeName.Grass)),
-        Pokemon(494, "Victini", "Une victoire a portee de main", 5, Type(TypeName.Fire, TypeName.Psychic)),
+class PokedexController(
+    private val pokemonService: PokemonService,
+) {
+    val pokemonDTOLists = mutableListOf(
+        PokemonDTO(
+            1,
+            "Bulbizarre",
+            "Un Pokemon bulbe vivant dans les champs",
+            1,
+            Type(TypeName.Grass, TypeName.Poison)
+        ),
+        PokemonDTO(152, "Germignon", "Un petit germe trop mignon", 2, Type(TypeName.Grass)),
+        PokemonDTO(494, "Victini", "Une victoire a portee de main", 5, Type(TypeName.Fire, TypeName.Psychic)),
     )
 
     @GetMapping
-    fun getAllPokemon(): ResponseEntity<List<Pokemon>> {
-        return ResponseEntity.ok().body(pokemonList)
+    fun getAllPokemon(): ResponseEntity<List<PokemonDTO>> {
+        return ResponseEntity.ok()
+            .body(pokemonService.getAllPokemon())
     }
 
     /**
      * Uncomment the following as you progress through the TD
      *
      *
+     *
     @GetMapping
     fun getPokemonById(
-        @RequestParam("id") id: Int
-    ): Pokemon {
-//TODO
+        @PathVariable id: Long
+    ): ResponseEntity<PokemonDTO> {
+        val pokemon = pokemonDTOLists.find { it.id == id }
+        if (pokemon != null) {
+            return ResponseEntity.ok().body(pokemon)
+        }
+        throw ResponseStatusException(HttpStatus.NOT_FOUND)
     }
 
+    @GetMapping("/generation/{generation}")
+    fun getPokemonByGeneration(
+        @PathVariable generation: Int
+    ): List<PokemonDTO> {
+        return pokemonDTOLists.filter { it.generation == generation }
+    }
 
     @PostMapping
     fun createPokemon(
-        @RequestBody pokemon: Pokemon
-    ): Unit {
-        //TODO
+        @RequestBody pokemonDTO: PokemonDTO
+    ): ResponseEntity<PokemonDTO> {
+        if (pokemonDTOLists.find { it.id == pokemonDTO.id } != null) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "the Pokemon already exists")
+        }
+        pokemonDTOLists.add(pokemonDTO)
+        return ResponseEntity(pokemonDTO, HttpStatus.CREATED)
+    }
+
+    @PatchMapping("/description/{id}")
+    fun editDescription(
+        @PathVariable id: Long,
+        @RequestBody description: String
+    ): ResponseEntity<PokemonDTO> {
+        val pokemonToUpdate = pokemonDTOLists.find { it.id == id }
+        if (pokemonToUpdate == null) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Pokemon with id: $id not found, update impossible")
+        }
+        val updatedPokemon = pokemonToUpdate.copy(description = description)
+        pokemonDTOLists.removeIf { it.id == id }
+        pokemonDTOLists.add(updatedPokemon)
+        return ResponseEntity.ok().body(updatedPokemon)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePokemon(
+        @PathVariable id: Long
+    ): ResponseEntity.HeadersBuilder<*> {
+        pokemonDTOLists.removeIf { it.id == id }
+        return ResponseEntity.noContent()
     }
     */
 }
--- TP/corriges/pokemon/src/main/kotlin/com/example/pokemon/service/PokemonService.kt
+++ TP/corriges/pokemon/src/main/kotlin/com/example/pokemon/service/PokemonService.kt
@@ -1,6 +1,16 @@
 package com.example.pokemon.service
 
+import com.example.pokemon.mappers.PokemonMapper
+import com.example.pokemon.model.PokemonDTO
+import com.example.pokemon.repository.PokemonRepository
 import org.springframework.stereotype.Service
 
 @Service
-class PokemonService
+class PokemonService(
+    private val pokemonRepository: PokemonRepository,
+    private val pokemonMapper: PokemonMapper
+) {
+    fun getAllPokemon(): List<PokemonDTO> =
+        pokemonRepository.findAll().map { pokemonMapper.pokemonEntityToPokemonDTO(it) }
+
+}
